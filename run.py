ef26d0bb6e52b357ed32ae5bc6133b677a2fd1e4
seria <seria.ati@gmail.com>
2026-01-19 15:42:57 +0900

    fix: Fix uvicorn logging integration

--- run.py
+++ run.py
@@ -9,4 +9,4 @@ from api.app import app
 
 if __name__ == "__main__":
     with suppress(KeyboardInterrupt, asyncio.CancelledError):
-        uvicorn.run(app, port=1078)
+        uvicorn.run(app, port=1078, log_config=None, log_level=None)
