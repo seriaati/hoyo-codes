1741caf637ab9471332bb78bad1e66164635b6ff
seria <seria.ati@gmail.com>
2026-01-22 00:00:00 +0900

    fix(parser): Fix GamesRadar parser

--- api/codes/parsers.py
+++ api/codes/parsers.py
@@ -23,17 +23,8 @@ def parse_gamesradar(content: str) -> list[tuple[str, str]]:
     soup = BeautifulSoup(content, "lxml")
     # find div with id article-body
     div = soup.find("div", id="article-body")
-    h2s = div.find_all("h2")
     uls = div.find_all("ul")
-    lis = []
-
-    for i, h2 in enumerate(h2s):
-        if "livestream" in h2.text.strip().lower() or h2.text.strip() in {
-            "Genshin Impact active codes",
-            "Honkai Star Rail codes",
-        }:
-            ul = uls[i]
-            lis.extend(ul.find_all("li"))
+    lis = [li for ul in uls for li in ul.find_all("li")]
 
     for li in lis:
         if li.strong is None or not li.strong.text.strip().isupper():
